f6ca40f12e963236344160eeccf7b69c36261600
Thor Whalen <thorwhalen1@gmail.com>
2023-06-09 16:45:45 +0200

    feat: add scrap/annotators.py

---
+++ creek/scrap/annotators.py
@@ -0,0 +1,73 @@
+"""Tools to make annotators.
+
+"""
+
+from typing import Union, Iterable, Tuple, Callable, KT, VT
+from operator import itemgetter
+from functools import partial
+from i2 import Pipe
+
+# ------------------- Types -------------------
+# TODO: Should we use the term KV (key-value-pair) instead of annotation?
+# TODO: Should we use some Time (numerical) type instead of KT here?
+IndexAnnot = Tuple[KT, VT]
+IndexAnnot.__doc__ = """An annotation whose key is an (time) index. 
+KT is usually numerical and represents time. 
+VT holds the value (info) of the annotation.
+"""
+
+Interval = Tuple[KT, KT]  # note this it's two KTs here, usually numerical.
+Interval.__doc__ = """An interval; i.e. a pair of indices"""
+
+IntervalAnnot = Tuple[Interval, VT]
+IntervalAnnot.__doc__ = """An annotation whose key is an interval."""
+
+KvExtractor = Callable[[Iterable], Iterable[IndexAnnot]]
+IntervalAnnot.__doc__ = """A function that extracts annotations from an iterable."""
+
+
+# ------------------- Annotators -------------------
+def track_intervals(indexed_tags: Iterable[IndexAnnot]) -> Iterable[IntervalAnnot]:
+    """Track intervals of tags in an iterable of indexed tags.
+
+    Example usage:
+
+    >>> iterable = ['a', 'b', 'a', 'b', 'c', 'c', 'd', 'd']
+    >>> list(track_intervals(enumerate(iterable)))
+    [((0, 2), 'a'), ((1, 3), 'b'), ((4, 5), 'c'), ((6, 7), 'd')]
+
+    """
+    open_tags = {}
+    for index, tag in indexed_tags:
+        if tag not in open_tags:
+            open_tags[tag] = index
+        else:
+            yield ((open_tags[tag], index), tag)
+            del open_tags[tag]
+
+
+only_interval = partial(map, itemgetter(0))
+
+
+def mk_interval_extractor(
+    *, kv_extractor: KvExtractor = enumerate, include_tag: bool = True
+) -> Callable[[Iterable], Iterable[Union[Interval, IntervalAnnot]]]:
+    """Make an interval extractor from a key-value extractor.
+
+    Example usage:
+
+    >>> iterable = ['a', 'b', 'a', 'b', 'c', 'c', 'd', 'd']
+    >>> extract_intervals = mk_interval_extractor()
+    >>> list(extract_intervals(iterable))
+    [((0, 2), 'a'), ((1, 3), 'b'), ((4, 5), 'c'), ((6, 7), 'd')]
+    >>> extract_intervals_without_tags = mk_interval_extractor(include_tag=False)
+    >>> assert list(extract_intervals_without_tags(iterable))
+    [(0, 2), (1, 3), (4, 5), (6, 7)]
+
+    See: 
+
+    """
+    interval_extractor = Pipe(KvExtractor, track_intervals)
+    if not include_tag:
+        interval_extractor = Pipe(interval_extractor, only_interval)
+    return interval_extractor
